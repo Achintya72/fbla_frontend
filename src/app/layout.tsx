b08adbdbad05a13de380739224aef159cb26edcc
Pranav Kannepalli <pranav.kannepalli@gmail.com>
2025-01-25 23:49:02 -0800

    feat: add job context and provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import { LoginContextProvider } from "@/repositories/loginContext";
+import { JobsContextProvider } from "@/repositories/jobsContext";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -23,8 +24,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} antialiased max-w-[1440px] mx-auto`}>
         <LoginContextProvider>
-          <Navbar />
-          {children}
+          <JobsContextProvider>
+            <Navbar />
+            {children}
+          </JobsContextProvider>
         </LoginContextProvider>
       </body>
     </html>
