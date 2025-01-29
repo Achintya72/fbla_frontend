5265767f29ea2b490774ec6a5629856422087898
Achintya72 <achintya.agrawal.90@gmail.com>
2025-01-28 21:03:12 -0800

    chore: creating mock db to make api configuration easier

---
+++ src/repositories/mockDB.tsx
@@ -0,0 +1,13 @@
+import { CounselorData } from "@/models/counselor";
+
+export const counselors: CounselorData[] = [
+    {
+        id: "3",
+        name: "Jane Smith",
+        email: "mtouran@lwsd.org",
+        students: ["1"],
+        endorsementRequests: ["1"],
+        jobReferences: [],
+        pendingRecruiters: ["2"],
+    }
+]
